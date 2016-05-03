b2fe798e73560996231357ddb057bb3b4bdef178
Nikolas Liolios <Nliolios@exeter.edu>
2016-05-03 11:29:37 -0400

    upload repo

--- analysis.py
+++ analysis.py
@@ -22,7 +22,7 @@ from datetime import datetime
 import os
 import sys
 
-# from matplotlib import pyplot
+from matplotlib import pyplot
 import numpy as np
 from lasagne import layers
 from nolearn.lasagne import BatchIterator
@@ -97,15 +97,15 @@ def plot_sample(x, y, axis):
         axis.scatter(y[0::2] * 48 + 48, y[1::2] * 48 + 48, marker='x', s=10)
 
 
-# def plot_weights(weights):
-#     fig = pyplot.figure(figsize=(6, 6))
-#     fig.subplots_adjust(
-#         left=0, right=1, bottom=0, top=1, hspace=0.05, wspace=0.05)
+def plot_weights(weights):
+    fig = pyplot.figure(figsize=(6, 6))
+    fig.subplots_adjust(
+        left=0, right=1, bottom=0, top=1, hspace=0.05, wspace=0.05)
 
-#     for i in range(16):
-#         ax = fig.add_subplot(4, 4, i + 1, xticks=[], yticks=[])
-#         ax.imshow(weights[:, i].reshape(96, 96), cmap='gray')
-#     pyplot.show()
+    for i in range(16):
+        ax = fig.add_subplot(4, 4, i + 1, xticks=[], yticks=[])
+        ax.imshow(weights[:, i].reshape(96, 96), cmap='gray')
+    pyplot.show()
 
 
 class FlipBatchIterator(BatchIterator):
@@ -366,47 +366,47 @@ def rebin(a, newshape):
     return a[tuple(indices)]
 
 
-# def plot_learning_curves(fname_specialists='net-specialists.pickle'):
-#     with open(fname_specialists, 'r') as f:
-#         models = pickle.load(f)
+def plot_learning_curves(fname_specialists='net-specialists.pickle'):
+    with open(fname_specialists, 'r') as f:
+        models = pickle.load(f)
 
-#     fig = pyplot.figure(figsize=(10, 6))
-#     ax = fig.add_subplot(1, 1, 1)
-#     ax.set_color_cycle(
-#         ['c', 'c', 'm', 'm', 'y', 'y', 'k', 'k', 'g', 'g', 'b', 'b'])
+    fig = pyplot.figure(figsize=(10, 6))
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_color_cycle(
+        ['c', 'c', 'm', 'm', 'y', 'y', 'k', 'k', 'g', 'g', 'b', 'b'])
 
-#     valid_losses = []
-#     train_losses = []
+    valid_losses = []
+    train_losses = []
 
-#     for model_number, (cg, model) in enumerate(models.items(), 1):
-#         valid_loss = np.array([i['valid_loss'] for i in model.train_history_])
-#         train_loss = np.array([i['train_loss'] for i in model.train_history_])
-#         valid_loss = np.sqrt(valid_loss) * 48
-#         train_loss = np.sqrt(train_loss) * 48
+    for model_number, (cg, model) in enumerate(models.items(), 1):
+        valid_loss = np.array([i['valid_loss'] for i in model.train_history_])
+        train_loss = np.array([i['train_loss'] for i in model.train_history_])
+        valid_loss = np.sqrt(valid_loss) * 48
+        train_loss = np.sqrt(train_loss) * 48
 
-#         valid_loss = rebin(valid_loss, (100,))
-#         train_loss = rebin(train_loss, (100,))
+        valid_loss = rebin(valid_loss, (100,))
+        train_loss = rebin(train_loss, (100,))
 
-#         valid_losses.append(valid_loss)
-#         train_losses.append(train_loss)
-#         ax.plot(valid_loss,
-#                 label='{} ({})'.format(cg[0], len(cg)), linewidth=3)
-#         ax.plot(train_loss,
-#                 linestyle='--', linewidth=3, alpha=0.6)
-#         ax.set_xticks([])
+        valid_losses.append(valid_loss)
+        train_losses.append(train_loss)
+        ax.plot(valid_loss,
+                label='{} ({})'.format(cg[0], len(cg)), linewidth=3)
+        ax.plot(train_loss,
+                linestyle='--', linewidth=3, alpha=0.6)
+        ax.set_xticks([])
 
-#     weights = np.array([m.output_num_units for m in models.values()],
-#                        dtype=float)
-#     weights /= weights.sum()
-#     mean_valid_loss = (
-#         np.vstack(valid_losses) * weights.reshape(-1, 1)).sum(axis=0)
-#     ax.plot(mean_valid_loss, color='r', label='mean', linewidth=4, alpha=0.8)
+    weights = np.array([m.output_num_units for m in models.values()],
+                       dtype=float)
+    weights /= weights.sum()
+    mean_valid_loss = (
+        np.vstack(valid_losses) * weights.reshape(-1, 1)).sum(axis=0)
+    ax.plot(mean_valid_loss, color='r', label='mean', linewidth=4, alpha=0.8)
 
-#     ax.legend()
-#     ax.set_ylim((1.0, 4.0))
-#     ax.grid()
-#     pyplot.ylabel("RMSE")
-#     pyplot.show()
+    ax.legend()
+    ax.set_ylim((1.0, 4.0))
+    ax.grid()
+    pyplot.ylabel("RMSE")
+    pyplot.show()
 
 
 if __name__ == '__main__':
